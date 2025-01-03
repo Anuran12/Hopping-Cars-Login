875aaeb9944b4339730ba5c6f7070fb85f9e2c12
Anuran12 <anuranarts@gmail.com>
2025-01-03 12:54:34 +0530

    setup navigation

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/appScreens/Home/Home';
+import OnBoarding from './src/screens/authentication/OnBoarding/OnBoarding';
+
+function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Onboarding" component={OnBoarding} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
