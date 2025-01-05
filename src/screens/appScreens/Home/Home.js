91f595cf02f2d9f3cdcb1d71455a5937dd9c5aed
Anuran12 <anuranarts@gmail.com>
2025-01-05 17:55:40 +0530

    add user name and image

--- src/screens/appScreens/Home/Home.js
+++ src/screens/appScreens/Home/Home.js
@@ -1,6 +1,7 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import {signOutUser} from '../../../utilities/Utilities';
+import auth from '@react-native-firebase/auth';
 
 const Home = () => {
   const handleSignout = () => {
@@ -11,10 +12,15 @@ const Home = () => {
       console.log(error);
     }
   };
+
+  const user = auth().currentUser;
   return (
     <View style={styles.main}>
       <Text style={{fontSize: 20}}>Welcome</Text>
-      <Text style={styles.title}>User</Text>
+      <Text style={styles.title}>
+        {user.displayName ? user.displayName : ''}
+      </Text>
+      <Image source={{uri: user.photoURL}} style={styles.avatar} />
       <TouchableOpacity style={styles.signout} onPress={() => handleSignout()}>
         <Text style={{color: 'white'}}>Sign Out</Text>
       </TouchableOpacity>
@@ -45,5 +51,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     borderRadius: 20,
     marginTop: 15,
+  },
+  avatar: {
+    height: 50,
+    width: 50,
   },
 });
