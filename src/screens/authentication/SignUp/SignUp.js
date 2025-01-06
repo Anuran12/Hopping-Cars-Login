b5ec2e4d41ac75c4e5eaa0dd69c7df16244feb26
Anuran12 <anuranarts@gmail.com>
2025-01-07 02:58:33 +0530

    sign up page design

--- src/screens/authentication/SignUp/SignUp.js
+++ src/screens/authentication/SignUp/SignUp.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   TouchableOpacity,
   ToastAndroid,
+  Image,
 } from 'react-native';
 import React, {useState} from 'react';
 import {
@@ -13,6 +14,10 @@ import {
   SignInWithFacebook,
   SignInWithGoogle,
 } from '../../../utilities/Utilities';
+import theme from '../../../styles/theme';
+import Logo from '../../../assets/images/Logo.png';
+import GoogleIcon from '../../../assets/icons/Google.png';
+import FacebookIcon from '../../../assets/icons/Facebook.png';
 
 const SignUp = ({navigation}) => {
   const [name, setName] = useState('');
@@ -47,103 +52,115 @@ const SignUp = ({navigation}) => {
     return errors;
   };
 
-  const SignUp = (email, password) => {
-    CreateAccountWithEmailandPassword({email, password, name})
-      .then(() => {
-        ToastAndroid.show('Account Created', ToastAndroid.SHORT);
-      })
-      .catch(error => {
-        if (error.code === 'auth/email-already-in-use') {
-          return setErrors({email: 'Email already in use'});
-        }
-        if (error.code === 'auth/invalid-email') {
-          return setErrors({email: 'Email id invalid'});
-        }
-        console.log(error);
-      });
-  };
-
   const handleSignup = () => {
     const validationErrors = getErrors(name, email, password, confirmPassword);
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
     } else {
       setErrors({});
-      SignUp(email, password);
-      console.log('Signup Successful');
-      // Proceed with sign-up logic
+      CreateAccountWithEmailandPassword({email, password, name})
+        .then(() => {
+          ToastAndroid.show('Account Created', ToastAndroid.SHORT);
+        })
+        .catch(error => {
+          if (error.code === 'auth/email-already-in-use') {
+            return setErrors({email: 'Email already in use'});
+          }
+          if (error.code === 'auth/invalid-email') {
+            return setErrors({email: 'Email id invalid'});
+          }
+          console.log(error);
+        });
     }
   };
 
   return (
     <View
-      style={{paddingHorizontal: 20, backgroundColor: 'white', height: '100%'}}>
-      <ScrollView>
-        <Text style={styles.title}>Welcome</Text>
-        <View style={{width: '100%'}}>
-          <View style={styles.form}>
-            <TextInput
-              placeholder="Enter Name"
-              placeholderTextColor="gray"
-              value={name}
-              onChangeText={setName}
-              style={styles.input}
-            />
-            {errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
+      style={{
+        paddingHorizontal: 20,
+        backgroundColor: theme.backgroundColor,
+        height: '100%',
+      }}>
+      <ScrollView
+        contentContainerStyle={styles.main}
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.logoContainer}>
+          <Image source={Logo} />
+          <Text style={{color: 'white', textAlign: 'right'}}>
+            Detailing Studio
+          </Text>
+        </View>
+        <Text style={styles.title}>Create Account</Text>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="Enter Name"
+            placeholderTextColor="gray"
+            value={name}
+            onChangeText={setName}
+            style={styles.input}
+          />
+          {errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
 
-            <TextInput
-              placeholder="Enter Email"
-              placeholderTextColor="gray"
-              keyboardType="email-address"
-              value={email}
-              onChangeText={setEmail}
-              style={styles.input}
-            />
-            {errors.email && (
-              <Text style={styles.errorText}>{errors.email}</Text>
-            )}
+          <TextInput
+            placeholder="Enter Email"
+            placeholderTextColor="gray"
+            keyboardType="email-address"
+            value={email}
+            onChangeText={setEmail}
+            style={styles.input}
+          />
+          {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
 
-            <TextInput
-              placeholder="Password"
-              placeholderTextColor="gray"
-              secureTextEntry={true}
-              value={password}
-              onChangeText={setPassword}
-              style={styles.input}
-            />
-            {errors.password && (
-              <Text style={styles.errorText}>{errors.password}</Text>
-            )}
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="gray"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={setPassword}
+            style={styles.input}
+          />
+          {errors.password && (
+            <Text style={styles.errorText}>{errors.password}</Text>
+          )}
 
-            <TextInput
-              placeholder="Confirm Password"
-              placeholderTextColor="gray"
-              secureTextEntry={true}
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-              style={styles.input}
-            />
-            {errors.confirmPassword && (
-              <Text style={styles.errorText}>{errors.confirmPassword}</Text>
-            )}
-          </View>
+          <TextInput
+            placeholder="Confirm Password"
+            placeholderTextColor="gray"
+            secureTextEntry={true}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            style={styles.input}
+          />
+          {errors.confirmPassword && (
+            <Text style={styles.errorText}>{errors.confirmPassword}</Text>
+          )}
+        </View>
 
-          <TouchableOpacity onPress={handleSignup} style={styles.signin}>
-            <Text style={styles.signinText}>Sign Up</Text>
-          </TouchableOpacity>
+        <TouchableOpacity onPress={handleSignup} style={styles.signin}>
+          <Text style={styles.signinText}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <View style={styles.continueWith}>
+          <View style={styles.line} />
+          <Text style={styles.orText}>Or Sign Up With</Text>
+          <View style={styles.line} />
+        </View>
+        <View style={styles.socialButtons}>
           <TouchableOpacity
             onPress={() =>
               SignInWithGoogle().then(() =>
                 ToastAndroid.show('Signed In', ToastAndroid.SHORT),
               )
             }
-            style={styles.signin}>
-            <Text style={styles.signinText}>Continue with Google</Text>
+            style={styles.signinwith}>
+            <Image source={GoogleIcon} style={styles.icon} />
+            <Text style={styles.signinText}>Google</Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => SignInWithFacebook()}
-            style={styles.signin}>
-            <Text style={styles.signinText}>Continue with Facebook</Text>
+            style={styles.signinwith}>
+            <Image source={FacebookIcon} style={styles.icon} />
+            <Text style={styles.signinText}>Facebook</Text>
           </TouchableOpacity>
         </View>
 
@@ -163,44 +180,98 @@ const SignUp = ({navigation}) => {
 export default SignUp;
 
 const styles = StyleSheet.create({
+  main: {
+    width: '100%',
+    paddingVertical: 30,
+    paddingTop: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+
   title: {
     fontSize: 30,
     textAlign: 'center',
+    color: 'white',
     marginVertical: 20,
-    marginBottom: 80,
-    color: 'black',
-    letterSpacing: 2,
-    fontWeight: '500',
   },
   form: {
     width: '100%',
     marginBottom: 20,
   },
   input: {
-    paddingVertical: 10,
+    paddingVertical: 15,
     paddingHorizontal: 20,
     marginVertical: 10,
     fontSize: 14,
     color: 'black',
-    borderRadius: 10,
-    backgroundColor: 'lightgray',
+    borderRadius: 25,
+    backgroundColor: 'transparent',
+    borderColor: theme.inputBorderColor,
+    borderWidth: 1,
   },
   signin: {
-    backgroundColor: 'black',
-    paddingVertical: 8,
+    backgroundColor: theme.primaryColor,
+    paddingVertical: 16,
     paddingHorizontal: 16,
-    borderRadius: 20,
-    marginTop: 15,
+    borderRadius: 40,
   },
   signinText: {
-    color: 'white',
+    color: '#222222',
     fontSize: 16,
     textAlign: 'center',
+    fontWeight: '700',
   },
   errorText: {
     fontSize: 14,
     color: 'red',
     marginTop: -8,
+  },
+  continueWith: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    marginVertical: 12,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: '#F2F2F2',
+    marginHorizontal: 10,
+  },
+  orText: {
+    fontSize: 14,
+    color: theme.textColor,
+    fontWeight: '600',
+  },
+  socialButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signinwith: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+    width: '45%',
+    backgroundColor: 'white',
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+    borderRadius: 40,
+  },
+  icon: {
+    width: 20,
+    height: 20,
   },
   footer: {
     paddingVertical: 15,
@@ -213,6 +284,8 @@ const styles = StyleSheet.create({
     color: 'gray',
   },
   linkText: {
-    color: 'blue',
+    fontWeight: '700',
+    color: theme.secondaryColor,
+    textDecorationLine: 'underline',
   },
 });
